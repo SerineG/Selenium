3bfe072ec403083e579e7e56bb3fa881cba2efc2
ragipbaylar <ragipbaylar86@gmail.com>
2023-02-20 21:32:00 -0500

    day9 end of Iframe

--- src/test/java/technostudyB7/day6/Example3.java
+++ src/test/java/technostudyB7/day6/Example3.java
@@ -1,11 +1,49 @@
 package technostudyB7.day6;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import technostudyB7.Utilities.UtilityClass;
+
+import java.time.Duration;
 
 public class Example3 extends UtilityClass{
     public static void main(String[] args) {
         driver.get("https://phptravels.net/");
+
+        WebElement accountButton = driver.findElement(By.id("ACCOUNT"));
+        accountButton.click();
+
+        WebElement customerSignUpButton = driver.findElement(By.xpath("//a[text()='Customer Signup']"));
+        customerSignUpButton.click();
+
+        WebElement nameInput = driver.findElement(By.name("first_name"));
+        nameInput.sendKeys("John");
+
+        WebElement lastName = driver.findElement(By.xpath("//input[@placeholder='Last Name']"));
+        lastName.sendKeys("Doe");
+
+        WebElement phone = driver.findElement(By.xpath("//input[@name='phone']"));
+        phone.sendKeys("6142859409");
+
+        WebElement email = driver.findElement(By.cssSelector("input[placeholder='Email']"));
+        email.sendKeys("gdgdgd@gmail.com");
+
+        WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
+        password.sendKeys("qwerty12345");
+
+//        WebElement iAnNotARobotCheckBox = driver.findElement(By.id("recaptcha-anchor"));
+//        iAnNotARobotCheckBox.click();
+
+        WebElement cookiesButton = driver.findElement(By.id("cookie_stop"));
+        cookiesButton.click();
+        WebElement signUpButton = driver.findElement(By.id("button"));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        wait.until(ExpectedConditions.elementToBeClickable(signUpButton));
+        signUpButton.click();
 
     }
 
